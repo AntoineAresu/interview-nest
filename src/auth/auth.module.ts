8c3b7fab3f48efcb28d68660cc5d3abe5714c583
AntoineAresu <you@example.com>
2022-05-05 10:09:50 +0200

    :sparkles: [Auth] Setup login route

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,11 @@ import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { LocalStrategy } from './local.strategy';
 import { PassportModule } from '@nestjs/passport';
+import { UsersService } from '../users/users.service';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   imports: [UsersModule, PassportModule],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, UsersService, PrismaService],
 })
 export class AuthModule {}
