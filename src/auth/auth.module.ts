d6ae9d6389fb70a45ecc16fecbc8661a51a704a9
AntoineAresu <you@example.com>
2022-05-05 10:23:33 +0200

    :sparkles: [Auth] Return jwt after login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,9 +5,18 @@ import { LocalStrategy } from './local.strategy';
 import { PassportModule } from '@nestjs/passport';
 import { UsersService } from '../users/users.service';
 import { PrismaService } from '../prisma.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: 'secret',
+      signOptions: { expiresIn: '15min' },
+    }),
+  ],
   providers: [AuthService, LocalStrategy, UsersService, PrismaService],
+  exports: [AuthService],
 })
 export class AuthModule {}
